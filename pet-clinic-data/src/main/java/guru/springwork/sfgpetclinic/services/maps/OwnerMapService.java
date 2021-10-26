1e4c8b5bd8c5673cac213600aa3a4018436afaf6
antonio-dumitrascu <antonio.dumitrascu@ibm.com>
2021-10-27 01:16:03 +0300

    Closes #6.

---
+++ pet-clinic-data/src/main/java/guru/springwork/sfgpetclinic/services/maps/OwnerMapService.java
@@ -0,0 +1,33 @@
+package guru.springwork.sfgpetclinic.services.maps;
+
+import guru.springwork.sfgpetclinic.models.Owner;
+import guru.springwork.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
