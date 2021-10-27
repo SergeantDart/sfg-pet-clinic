3a245a3d9a690e8ae4c324176d179cb013713052
antonio-dumitrascu <antonio.dumitrascu@ibm.com>
2021-10-27 15:35:03 +0300

    Closes #22.

--- pet-clinic-web/src/main/java/guru/springwork/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springwork/sfgpetclinic/controllers/VetController.java
@@ -1,15 +1,24 @@
 package guru.springwork.sfgpetclinic.controllers;
 
-import guru.springwork.sfgpetclinic.models.Vet;
-import guru.springwork.sfgpetclinic.services.CrudService;
+import guru.springwork.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "vets/index", "vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
