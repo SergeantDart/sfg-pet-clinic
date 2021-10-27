46fec536ebeba7af8ad14742d6d48ca332a81423
antonio-dumitrascu <antonio.dumitrascu@ibm.com>
2021-10-27 15:08:11 +0300

    Closes #15.

--- pet-clinic-web/src/main/java/guru/springwork/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springwork/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package guru.springwork.sfgpetclinic.controllers;
 
+import guru.springwork.sfgpetclinic.models.Owner;
+import guru.springwork.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/", "", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
