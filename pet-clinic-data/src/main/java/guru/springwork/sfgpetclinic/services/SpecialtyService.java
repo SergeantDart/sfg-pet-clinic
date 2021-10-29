8ad2c6c0b0f841c263e465eb6d6f10335f13fb96
antonio-dumitrascu <antonio.dumitrascu@ibm.com>
2021-10-29 20:56:08 +0300

    Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springwork/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package guru.springwork.sfgpetclinic.services;
+
+import guru.springwork.sfgpetclinic.models.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
