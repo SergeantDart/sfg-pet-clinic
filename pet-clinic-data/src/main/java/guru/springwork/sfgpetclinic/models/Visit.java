1c0386b967300e281e06fab5d7526f7fae88bd1c
antonio-dumitrascu <antonio.dumitrascu@ibm.com>
2021-10-31 13:06:51 +0200

    Closes #38.

--- pet-clinic-data/src/main/java/guru/springwork/sfgpetclinic/models/Visit.java
+++ pet-clinic-data/src/main/java/guru/springwork/sfgpetclinic/models/Visit.java
@@ -1,11 +1,20 @@
 package guru.springwork.sfgpetclinic.models;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
@@ -31,4 +40,5 @@ public class Visit extends BaseEntity {
     public void setPet(Pet pet) {
         this.pet = pet;
     }
+
 }
