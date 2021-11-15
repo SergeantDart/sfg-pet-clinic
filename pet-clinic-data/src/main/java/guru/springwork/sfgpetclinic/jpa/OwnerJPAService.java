d97dfa6d6fe24ede4d35c1c4a2d088546917cc6c
antonio-dumitrascu <antonio.dumitrascu@ibm.com>
2021-11-16 01:06:04 +0200

    Maven - modifications

---
+++ pet-clinic-data/src/main/java/guru/springwork/sfgpetclinic/jpa/OwnerJPAService.java
@@ -0,0 +1,62 @@
+package guru.springwork.sfgpetclinic.jpa;
+
+import guru.springwork.sfgpetclinic.model.Owner;
+import guru.springwork.sfgpetclinic.repository.OwnerRepository;
+import guru.springwork.sfgpetclinic.repository.PetRepository;
+import guru.springwork.sfgpetclinic.repository.PetTypeRepository;
+import guru.springwork.sfgpetclinic.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class OwnerJPAService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    @Autowired
+    public OwnerJPAService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springwork/sfgpetclinic/jpa/PetJPAService.java
@@ -0,0 +1,48 @@
+package guru.springwork.sfgpetclinic.jpa;
+
+import guru.springwork.sfgpetclinic.model.Pet;
+import guru.springwork.sfgpetclinic.repository.PetRepository;
+import guru.springwork.sfgpetclinic.service.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetJPAService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
